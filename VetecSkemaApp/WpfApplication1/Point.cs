7f8b0f335997a021ef9fd839b4214ecf6ee3a0bd
TBroedsgaard <troels@broedsgaard.com>
2014-02-06 14:00:35 +0100

    added drawing

---
+++ VetecSkemaApp/WpfApplication1/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1
+{
+    class Point
+    {
+        public uint X { get; set; }
+        public uint Y { get; set; }
+
+        public Point(uint x, uint y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
